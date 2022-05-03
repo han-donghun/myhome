0b452b63c24e66b0157461a08ce4589c22894f14
hans <jbruno@korea.kr>
2022-05-03 23:05:19 +0900

    jpa rest service

--- src/main/java/com/hans/myhome/repository/BoardRepository.java
+++ src/main/java/com/hans/myhome/repository/BoardRepository.java
@@ -3,5 +3,10 @@ package com.hans.myhome.repository;
 import com.hans.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
